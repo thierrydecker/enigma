2c849c4bf55b4de0e09c5f9fca3fd2ea499cceea
Thierry DECKER <mail@thierry-decker.com>
2019-08-27 09:24:36 +0200

    Initial commit

---
+++ enigma/core.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+def main():
+    """The main function"""
+    pass
+
+
+if __name__ == "__main":
+    main()
---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license_document = f.read()
+
+setup(
+        name='enigma',
+        version='0.1.0',
+        description='Enigma implementation',
+        long_description=readme,
+        author='Thierry DECKER',
+        author_email='mail@thierry-decker.com',
+        url='https://github.com/thierrydecker/enigma',
+        license=license_document,
+        packages=find_packages(exclude=('tests', 'docs'))
+)
